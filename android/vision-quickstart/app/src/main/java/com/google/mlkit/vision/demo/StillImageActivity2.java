a0690184a4fcaa07f4a3c6610064f2a2eb39d52d
williamlimas <68885232+williamlimas@users.noreply.github.com>
2020-10-14 14:06:46 +0700

    utk demo pakai facenet

--- android/vision-quickstart/app/src/main/java/com/google/mlkit/vision/demo/StillImageActivity2.java
+++ android/vision-quickstart/app/src/main/java/com/google/mlkit/vision/demo/StillImageActivity2.java
@@ -524,7 +524,7 @@ public final class StillImageActivity2 extends AppCompatActivity {
 //        Log.d("STATE2", "top : "+ top + " bottom : "+ bottom +" left : "+ left +" right : "+ right );
 
         Bitmap new_bitmap = Bitmap.createBitmap(resizedBitmap,left,top,right-left,bottom-top);
-        Bitmap new_bitmap_resized = Bitmap.createScaledBitmap(new_bitmap,112,112,true);
+        Bitmap new_bitmap_resized = Bitmap.createScaledBitmap(new_bitmap,160,160,true);
 
         saveToInternalStorage(new_bitmap_resized, 2 );
         preview.setImageBitmap(new_bitmap_resized);
